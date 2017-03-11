56a786b9bef76ba26df184c9244e8b53a178430e
IMM9O <islam.muhamed.90+github@gmail.com>
2017-03-11 22:21:02 +0200

    first commit

---
+++ src/app/album-list/album-list.component.ts
@@ -0,0 +1,73 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-album-list',
+  templateUrl: './album-list.component.html',
+  styleUrls: ['./album-list.component.css']
+})
+export class AlbumListComponent implements OnInit {
+  albums: any[]= [{name: '' , year: ''} ,
+   {name: '' , year: ''} ,
+   {name: '' , year: ''} ,
+   {name: '' , year: ''} ,
+   {name: '' , year: ''},
+    {name: '' , year: ''} ,
+    {name: '' , year: ''} ,
+    {name: '' , year: ''} ,
+    {name: '' , year: ''} ,
+    {name: '' , year: ''} ,
+    {name: '' , year: ''} ,
+    {name: '' , year: ''} ,
+    {name: '' , year: ''} ,
+    {name: '' , year: ''} ,
+    {name: '' , year: ''} ,
+    {name: '' , year: ''} ,
+    {name: '' , year: ''} ,
+    {name: '' , year: ''} ,
+    {name: '' , year: ''} ,
+    {name: '' , year: ''} ,
+    {name: '' , year: ''} ,
+    {name: '' , year: ''} ,
+    {name: '' , year: ''} ,
+    {name: '' , year: ''} ,
+    {name: '' , year: ''} ,
+    {name: '' , year: ''} ,
+    {name: '' , year: ''} ,
+    {name: '' , year: ''} ,
+    {name: '' , year: ''} ,
+    {name: '' , year: ''} ,
+    {name: '' , year: ''} ,
+    {name: '' , year: ''} ,
+    {name: '' , year: ''} ,
+    {name: '' , year: ''} ,
+    {name: '' , year: ''} ,
+    {name: '' , year: ''} ,
+    {name: '' , year: ''} ,
+    {name: '' , year: ''} ,
+    {name: '' , year: ''} ,
+    {name: '' , year: ''} ,
+    {name: '' , year: ''} ,
+    {name: '' , year: ''} ,
+    {name: '' , year: ''} ,
+    {name: '' , year: ''} ,
+    {name: '' , year: ''} ,
+    {name: '' , year: ''} ,
+    {name: '' , year: ''} ,
+    {name: '' , year: ''} ,
+    {name: '' , year: ''} ,
+    {name: '' , year: ''} ,
+    {name: '' , year: ''} ,
+    {name: '' , year: ''} ,
+    {name: '' , year: ''} ,
+    {name: '' , year: ''} ,
+  {name: '' , year: ''} ,
+  {name: '' , year: ''} ,
+  {name: '' , year: ''} ,
+  {name: '' , year: ''} ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,34 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {ArtistComponent} from './artist/artist.component';
+import {AlbumComponent} from './album/album.component';
+import {Routes, RouterModule} from '@angular/router';
+import {AboutComponent} from './about/about.component';
+import { AlbumListComponent } from './album-list/album-list.component';
+
+const appRoutes: Routes = [
+  {
+    path: 'about',
+    component: AboutComponent
+  }, {
+    path: 'album/:id',
+    component: AlbumComponent
+  }, {
+    path: 'artist/:id',
+    component: ArtistComponent
+  }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, ArtistComponent, AlbumComponent, AboutComponent, AlbumListComponent
   ],
   imports: [
+    RouterModule.forRoot(appRoutes),
     BrowserModule,
     FormsModule,
     HttpModule
@@ -17,4 +36,4 @@ import { AppComponent } from './app.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}

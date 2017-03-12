6a495546b409238f60fbd5a4073a3d544e890dc0
IMM9O <islam.muhamed.90+github@gmail.com>
2017-03-12 09:04:48 +0200

    add track play and pause logic

--- src/app/album/album.component.ts
+++ src/app/album/album.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit , OnDestroy } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
 import { SpotifyService } from './../spotify.service';
 
@@ -7,8 +7,11 @@ import { SpotifyService } from './../spotify.service';
   templateUrl: './album.component.html',
   styleUrls: ['./album.component.css']
 })
-export class AlbumComponent implements OnInit {
+export class AlbumComponent implements OnInit , OnDestroy{
   album: any = null;
+  playUrl = '';
+  audio: any = null;
+  playing = false;
 
   constructor(private _route: ActivatedRoute , private _spotifyService: SpotifyService) {
        _spotifyService.currentAlubumsSongs$.subscribe( res => this.album = res );
@@ -16,6 +19,35 @@ export class AlbumComponent implements OnInit {
 
   ngOnInit() {
     this._route.params.subscribe((params: any) => this._spotifyService.setAlbumId(params.id));
+  }
+
+  ngOnDestroy() {
+      if ( this.playing ){
+        this.audio.pause();
+      }
+  }
+
+  playAudio(_url: string) {
+        const audio = new Audio(_url);
+        if (!this.playing) {
+            audio.play();
+            this.playUrl = _url;
+            this.audio = audio;
+            this.playing = true;
+        }else {
+            if(_url === this.playUrl ) {
+                this.audio.pause();
+                this.playing = false;
+            }else {
+                this.audio.pause();
+                audio.play();
+                this.playUrl = _url;
+                this.audio = audio;
+                this.playing = true;
+            }
+
+        }
+
   }
 
 }

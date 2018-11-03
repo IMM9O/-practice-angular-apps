f1498457b82180ad636a758b08073b99777cf81f
IMM9O <islam.muhamed.90+github@gmail.com>
2018-11-03 12:23:38 +0200

    lint code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
 import { CoreModule } from './core/core.module';
 import { RootStoreModule } from './root-store/root-store.module';

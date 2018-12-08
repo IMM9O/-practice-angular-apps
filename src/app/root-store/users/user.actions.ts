470c577c4770462b76fc04eaedb7cb1de91a06c1
IMM9O <islam.muhamed.90+github@gmail.com>
2018-12-08 20:07:51 +0200

    lint code

--- src/app/root-store/users/user.actions.ts
+++ src/app/root-store/users/user.actions.ts
@@ -1,6 +1,10 @@
 import { Action } from '@ngrx/store';
 import { IUser } from 'src/models/IUser';
 
+/**
+ * For each action type in an action group, make a simple
+ * enum object for all of this group's action types.
+ */
 export enum UserActionTypes {
   USERS_RECEIVED = 'USERS_RECEIVED',
   USER_RECEIVED = 'USER_RECEIVED',
@@ -8,6 +12,12 @@ export enum UserActionTypes {
   USER_DELETED = 'USER_DELETED',
   RESET = 'RESET'
 }
+
+/**
+ * Every action is comprised of at least a type and an optional
+ * payload. Expressing actions as classes enables powerful
+ * type checking in reducer functions.
+ */
 
 export class UsersDetailsReceived implements Action {
   readonly type = UserActionTypes.USERS_RECEIVED;
@@ -29,7 +39,10 @@ export class UserDeletedReceived implements Action {
 export class Reset implements Action {
   readonly type = UserActionTypes.RESET;
 }
-
+/**
+ * Export a type alias of all actions in this action group
+ * so that reducers can easily compose action types
+ */
 export type UserActions =
   | UsersDetailsReceived
   | UserDetailsReceived

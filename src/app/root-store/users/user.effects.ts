d377f8e17dde21f8e42378e8de2df2457f60192e
IMM9O <islam.muhamed.90+github@gmail.com>
2019-08-31 12:16:53 +0200

    Upgrade to angular v8

--- src/app/root-store/users/user.effects.ts
+++ src/app/root-store/users/user.effects.ts
@@ -1,51 +1,47 @@
 import { Injectable } from '@angular/core';
-import { Effect, Actions } from '@ngrx/effects';
+import { Effect, Actions, ofType } from '@ngrx/effects';
+import { map } from 'rxjs/operators';
 import { ToastrService } from 'ngx-toastr';
 
 import * as UserActions from './user.actions';
-import { map } from 'rxjs/operators';
 
 @Injectable()
 export class UserEffects {
   // Create
   @Effect({ dispatch: false })
-  created$ = this.action$
-    .ofType(UserActions.UserActionTypes.USER_RECEIVED)
-    .pipe(
-      map((action: UserActions.UserDetailsReceived) =>
-        this.toastr.success(`User ${action.payload.first_name} Created`)
-      )
-    );
+  created$ = this.action$.pipe(
+    ofType(UserActions.UserActionTypes.USER_RECEIVED),
+    map((action: UserActions.UserDetailsReceived) =>
+      this.toastr.success(`User ${action.payload.first_name} Created`)
+    )
+  );
 
   // Read
   @Effect({ dispatch: false })
-  userRecived$ = this.action$
-    .ofType(UserActions.UserActionTypes.USERS_RECEIVED)
-    .pipe(
-      map((action: UserActions.UsersDetailsReceived) =>
-        this.toastr.success('New Users List add')
-      )
-    );
+  userRecived$ = this.action$.pipe(
+    ofType(UserActions.UserActionTypes.USERS_RECEIVED),
+    map((action: UserActions.UsersDetailsReceived) =>
+      this.toastr.success('New Users List add')
+    )
+  );
 
   // Update
   @Effect({ dispatch: false })
-  updated$ = this.action$
-    .ofType(UserActions.UserActionTypes.USER_UPDATED)
-    .pipe(
-      map((action: UserActions.UserUpdatedReceived) =>
-        this.toastr.success(`User ${action.payload.first_name} Updated`)
-      )
-    );
+  updated$ = this.action$.pipe(
+    ofType(UserActions.UserActionTypes.USER_UPDATED),
+    map((action: UserActions.UserUpdatedReceived) =>
+      this.toastr.success(`User ${action.payload.first_name} Updated`)
+    )
+  );
 
   // Delete
   @Effect({ dispatch: false })
-  deleted$ = this.action$
-    .ofType(UserActions.UserActionTypes.USER_DELETED)
-    .pipe(
-      map((action: UserActions.UserDeletedReceived) =>
-        this.toastr.success('User Delted')
-      )
-    );
+  deleted$ = this.action$.pipe(
+    ofType(UserActions.UserActionTypes.USER_DELETED),
+    map((action: UserActions.UserDeletedReceived) =>
+      this.toastr.success('User Delted')
+    )
+  );
 
   constructor(private action$: Actions, private toastr: ToastrService) {}
 }
